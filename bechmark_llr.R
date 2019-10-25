39b814e72ace678df9498cf2f3f12d3ede656274
“Ling <tan0.addy@gmail.com>
2019-10-25 11:08:05 -0400

    bechmark correct file

---
+++ bechmark_llr.R
@@ -0,0 +1,12 @@
+install.packages("bench")
+library(bench)
+
+#setwd("..")
+#dir<-getwd()
+#setwd(toString(paste(dir,"/Stat610/",sep='')))
+setwd("/Users/addytan/Stat610/")
+
+bnchmark <- bench::mark(
+  source("llr_functions.R"))
+bnchmark
+
